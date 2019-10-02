f4ec8f2b694d1f78e6f7e850beb111622e2586ea
MatheusEstanislau <matheus.estanislau7@gmail.com>
2019-10-02 11:43:14 -0300

    Criado Servidor.

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.json({ message: 'Hello Wolrd' }));
 
 app.listen(3333);
