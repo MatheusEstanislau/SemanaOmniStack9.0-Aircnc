dea40c57d7fd47eb73b20dbd4826533ffac9ee03
MatheusEstanislau <matheus.estanislau7@gmail.com>
2019-10-02 12:51:05 -0300

    Criados conexao com banco de dados e models contendo UserChema.

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://OmniStack:ame@cluster0-8pxjy.mongodb.net/semana09?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // GET, POST, PUT, DELETE
 // req.query = Acessar query params(para filtros)
 // req.params = Acessar rout params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
+
 app.use(express.json());
 app.use(routes);
 
