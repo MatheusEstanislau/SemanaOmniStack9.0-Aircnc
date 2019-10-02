a47798f54eac6a1abe66761193ca0c2792e77325
MatheusEstanislau <matheus.estanislau7@gmail.com>
2019-10-02 13:09:11 -0300

    Criado SessionController.

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/users', (req, res) => res.json(req.body));
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
