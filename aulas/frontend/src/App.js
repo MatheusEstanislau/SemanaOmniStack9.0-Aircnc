90bac08757c64afc6d6712d7c3a18b99bdbd060c
MatheusEstanislau <matheus.estanislau7@gmail.com>
2019-10-03 06:05:10 -0300

    Adicionado axios, criado arquivo api.js e atualizada página App.js.

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState }from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
-  return (
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const response = await api.post('/sessions', { email });
+
+        const { _id } = response.data;
+
+        localStorage.setItem('user', _id );
+    }
+
+    return (
     <div className={"container"}>
       <img src={logo}  alt="AirCnC"/>
 
@@ -12,12 +25,13 @@ function App() {
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor={"email"}>E-MAIL *</label>
           <input
             id={"email"}
             type={"email"}
             placeholder={"Seu melhor e-mail"}
+            onChange={event => setEmail(event.target.value)}
           />
           <button className={"btn"} type={"submit"}>Entrar</button>
         </form>
