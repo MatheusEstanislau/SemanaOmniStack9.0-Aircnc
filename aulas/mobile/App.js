94bffdf0b26f5a016926feb98a98313fbe4020d9
MatheusEstanislau <matheus.estanislau7@gmail.com>
2019-10-05 08:47:34 -0300

    Tela de login Estilizada.

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Omnistack!</Text>
-      <Text style={styles.text}>Hello Omnistack!</Text>
-    </View>
+    <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: '#000'
-  }
-});
